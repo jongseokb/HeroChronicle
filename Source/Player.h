c433760707348d8b09c075c344103e7d655349cc
Jongseokb <Jongseokb@gmail.com>
2025-12-15 18:43:06 +0900

    헤더파일들 추가
    스탯 정보, 클래스 정보 추가

---
+++ Source/Player.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+// 플레이어 종류
+enum class PlayerType
+{
+	PT_Knight = 1,	// 기사
+	PT_Archor,		// 궁수
+	PT_Wizard		// 마법사
+};
---
+++ Source/StatInfo.h
@@ -0,0 +1,13 @@
+﻿// Project Start Date: 2025.12.15
+// Copyright (c) 2025 HeroChronicle Project by Jongseokb.
+
+#pragma once
+
+// 스탯 정보
+struct StatInfo
+{
+	int Hp; // 체력
+	int Mp; // 마나
+	int AttackDamage; // 물리 공격력
+	int PhysicalDefense; // 물리 방어력
+};
